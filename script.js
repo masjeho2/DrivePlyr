020a67a2f480223ff1ab033ae7ecc711a141be8f
masjeho2 <126146946+masjeho2@users.noreply.github.com>
2023-12-11 23:46:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,12 +38,12 @@ let getbase=()=>{
 
 
 //Player Openers (diff. func. coding intentionally)
-let opensp=()=> { window.open('https://driveplyr.sh20raj.repl.co/sopplayer.html?id='+base)}
-let openplyr=()=> { window.open('https://driveplyr.sh20raj.repl.co/plyr.html?id='+base)}
-let openfluid=()=> { window.open('https://driveplyr.sh20raj.repl.co/fluid.html?id='+base)}
-let openafterglow=()=> { window.open('https://driveplyr.sh20raj.repl.co/afterglow.html?id='+base)}
-let openmediaelements=()=> { window.open('https://driveplyr.sh20raj.repl.co/mediaelements.html?id='+base)}
-let openvlitejs=()=> { window.open('https://driveplyr.sh20raj.repl.co/vlitejs.html?id='+base)}
+let opensp=()=> { window.open('https://play.oneplay.bio/sopplayer.html?id='+base)}
+let openplyr=()=> { window.open('https://play.oneplay.bio/plyr.html?id='+base)}
+let openfluid=()=> { window.open('https://play.oneplay.bio/fluid.html?id='+base)}
+let openafterglow=()=> { window.open('https://play.oneplay.bio/afterglow.html?id='+base)}
+let openmediaelements=()=> { window.open('https://play.oneplay.bio/mediaelements.html?id='+base)}
+let openvlitejs=()=> { window.open('https://play.oneplay.bio/vlitejs.html?id='+base)}
 
 
 let pmsg = 'Copy Embed Code' ;
@@ -51,7 +51,7 @@ let pmsg = 'Copy Embed Code' ;
 let embedsp=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://driveplyr.sh20raj.repl.co/sopplayer.html?id=${base}" 
+src="https://play.oneplay.bio/sopplayer.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -59,7 +59,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedplyr=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://driveplyr.sh20raj.repl.co/plyr.html?id=${base}" 
+src="https://play.oneplay.bio/plyr.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -67,7 +67,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedfluid=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://driveplyr.sh20raj.repl.co/fluid.html?id=${base}" 
+src="https://play.oneplay.bio/fluid.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -75,7 +75,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedafterglow=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://driveplyr.sh20raj.repl.co/afterglow.html?id=${base}" 
+src="https://play.oneplay.bio/afterglow.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -83,7 +83,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedmediaelements=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://driveplyr.sh20raj.repl.co/mediaelements.html?id=${base}" 
+src="https://play.oneplay.bio/mediaelements.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -91,7 +91,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedvlitejs=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://driveplyr.sh20raj.repl.co/vlitejs.html?id=${base}" 
+src="https://play.oneplay.bio/vlitejs.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`);
@@ -100,13 +100,13 @@ gyroscope; picture-in-picture" allowfullscreen>
 
 
 let iframe=()=> {
-  s('afterglow').src= 'https://driveplyr.sh20raj.repl.co/afterglow.html?id='+base;
-  s('fluid').src='https://driveplyr.sh20raj.repl.co/fluid.html?id='+base;
-  s('plyr').src='https://driveplyr.sh20raj.repl.co/plyr.html?id='+base;
+  s('afterglow').src= 'https://play.oneplay.bio/afterglow.html?id='+base;
+  s('fluid').src='https://play.oneplay.bio/fluid.html?id='+base;
+  s('plyr').src='https://play.oneplay.bio/plyr.html?id='+base;
 
-  s('vlitejs').src='https://driveplyr.sh20raj.repl.co/vlitejs.html?id='+base;
-  s('mediaelements').src='https://driveplyr.sh20raj.repl.co/mediaelements.html?id='+base;
+  s('vlitejs').src='https://play.oneplay.bio/vlitejs.html?id='+base;
+  s('mediaelements').src='https://play.oneplay.bio/mediaelements.html?id='+base;
   
-    s('sopplayer').src='https://driveplyr.sh20raj.repl.co/sopplayer.html?id='+base;
+    s('sopplayer').src='https://play.oneplay.bio/sopplayer.html?id='+base;
   
 }
